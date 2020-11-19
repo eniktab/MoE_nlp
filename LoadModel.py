ce1daa05cfeef6a0a3d8da3273b5050ac57e456d
Eliatan Niktab <maniktab@gmail.com>
2020-11-20 04:37:09 +1300

    Create LoadModel.py
    change path and load values from free bucket

---
+++ LoadModel.py
@@ -0,0 +1,137 @@
+import tensorflow as tf
+import numpy as np
+import pickle
+
+
+def calc_max_length(tensor):
+    return max(len(t) for t in tensor)
+
+
+def load_image(image_path):
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, (299, 299))
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    return img, image_path
+
+image_model = tf.keras.applications.InceptionV3(include_top=False,
+                                                weights='imagenet')
+new_input = image_model.input
+hidden_layer = image_model.layers[-1].output
+image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+
+class BahdanauAttention(tf.keras.Model):
+  def __init__(self, units):
+    super(BahdanauAttention, self).__init__()
+    self.W1 = tf.keras.layers.Dense(units)
+    self.W2 = tf.keras.layers.Dense(units)
+    self.V = tf.keras.layers.Dense(1)
+
+  def call(self, features, hidden):
+    hidden_with_time_axis = tf.expand_dims(hidden, 1)
+    attention_hidden_layer = (tf.nn.tanh(self.W1(features) +
+                                         self.W2(hidden_with_time_axis)))
+    score = self.V(attention_hidden_layer)
+    attention_weights = tf.nn.softmax(score, axis=1)
+    context_vector = attention_weights * features
+    context_vector = tf.reduce_sum(context_vector, axis=1)
+
+    return context_vector, attention_weights
+
+class CNN_Encoder(tf.keras.Model):
+    def __init__(self, embedding_dim):
+        super(CNN_Encoder, self).__init__()
+        self.fc = tf.keras.layers.Dense(embedding_dim)
+
+    def call(self, x):
+        x = self.fc(x)
+        x = tf.nn.relu(x)
+        return x
+
+class RNN_Decoder(tf.keras.Model):
+  def __init__(self, embedding_dim, units, vocab_size):
+    super(RNN_Decoder, self).__init__()
+    self.units = units
+
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(self.units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    self.fc1 = tf.keras.layers.Dense(self.units)
+    self.fc2 = tf.keras.layers.Dense(vocab_size)
+
+    self.attention = BahdanauAttention(self.units)
+
+  def call(self, x, features, hidden):
+    context_vector, attention_weights = self.attention(features, hidden)
+    x = self.embedding(x)
+    x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+    output, state = self.gru(x)
+    x = self.fc1(output)
+    x = tf.reshape(x, (-1, x.shape[2]))
+    x = self.fc2(x)
+    return x, state, attention_weights
+
+  def reset_state(self, batch_size):
+    return tf.zeros((batch_size, self.units))
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+def loss_function(real, pred):
+  mask = tf.math.logical_not(tf.math.equal(real, 0))
+  loss_ = loss_object(real, pred)
+
+  mask = tf.cast(mask, dtype=loss_.dtype)
+  loss_ *= mask
+
+  return tf.reduce_mean(loss_)
+
+
+embedding_dim = 256
+units = 512
+vocab_size = 3000 + 1
+attention_features_shape = 64
+tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
+                                                  oov_token="<unk>",
+                                                  filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+with open("/home/niktabel/PycharmProjects/deepeli/train_captions", 'rb') as pickle_file:
+    train_captions = pickle.load(pickle_file)
+
+tokenizer.fit_on_texts(train_captions)
+train_seqs = tokenizer.texts_to_sequences(train_captions)
+max_length = calc_max_length(train_seqs)
+
+encoder = CNN_Encoder(embedding_dim)
+decoder = RNN_Decoder(embedding_dim, units, vocab_size)
+optimizer = tf.keras.optimizers.Adam()
+
+checkpoint_path = "/home/niktabel/PycharmProjects/deepeli/checkpoints/train"
+ckpt = tf.train.Checkpoint(encoder=encoder,
+                           decoder=decoder,
+                           optimizer = optimizer)
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+ckpt.restore(ckpt_manager.latest_checkpoint)
+
+def evaluate(image):
+    hidden = decoder.reset_state(batch_size=1)
+    temp_input = tf.expand_dims(load_image(image_path)[0], 0)
+    img_tensor_val = image_features_extract_model(temp_input)
+    img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+    features = encoder(img_tensor_val)
+    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    result = []
+
+    for i in range(max_length):
+        predictions, hidden, attention_weights = decoder(dec_input, features, hidden)
+        predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
+        result.append(tokenizer.index_word[predicted_id])
+        if tokenizer.index_word[predicted_id] == '<end>':
+            return result
+        dec_input = tf.expand_dims([predicted_id], 0)
+    return result
+
+image_path = '/home/niktabel/PycharmProjects/deepeli/train2014/COCO_train2014_000000563264.jpg'
+evaluate(image_path)
